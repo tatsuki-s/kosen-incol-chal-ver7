505c2d5dd0864d8db3f16559c9c05f7cd88cd846
tatsuki-s <zekoushn@gmail.com>
2026-02-22 00:37:17 +0900

    ドラッグ範囲を限定

--- drag_game.gd
+++ drag_game.gd
@@ -7,8 +7,8 @@ extends Node2D
 var dragIdx = [0, 0]
 var isDraging = false
 var target = [0, 0]
-var dragSpace = 32
-var area = Rect2(32, 32, 1216, 656)
+var dragSpace = 64
+var area = Rect2(64, 64, 1152, 576)
 
 func _ready() -> void:
 	var screen = get_parent().screen
@@ -16,11 +16,15 @@ func _ready() -> void:
 
 func _input(event):
 	if event is InputEventScreenDrag:
-		var localPos = get_local_mouse_position()
+		var localPos = get_viewport().get_mouse_position()
+		var posX = snapped(localPos.x, dragSpace)
+		var posY = snapped(localPos.y, dragSpace)
 		
-		var pos = event.position
-		print(int(pos[0])," ", int(pos[1]))
-		if area.has_point(Vector2(pos[0], pos[1])):
-			parts.position.x = int(localPos.x/dragSpace)*dragSpace
-			parts.position.y = int(localPos.y/dragSpace)*dragSpace
+		posX = clamp(posX, area.position.x, area.end.x)
+		posY = clamp(posY, area.position.y, area.end.y)
+		
+		var camPos = get_viewport().get_canvas_transform().affine_inverse() * Vector2(posX, posY)
+		
+		parts.global_position = camPos
+		print(camPos)
 		
