e37a839373d535444e3ddaf71304bc8088d9a4be
tatsuki-s <zekoushn@gmail.com>
2026-02-22 18:29:28 +0900

    ドラッグ位置の初期化

--- drag_game.gd
+++ drag_game.gd
@@ -1,22 +1,37 @@
 extends Node2D
 
 var partsScene = preload("res://parts.tscn")
+var space = 96
 
-var ans = Vector2(-192.0, 280.0)
+var ans = [
+	Vector2(-192.0, 280.0),
+	Vector2(-192.0, 280.0),
+	Vector2(-192.0, 280.0),
+	Vector2(-192.0, 280.0),
+	Vector2(-192.0, 280.0)
+	]
 
 
 func _ready() -> void:
-	spawn(Vector2(0, 0))
-	
-func spawn(pos):
+	#spawn(Vector2(0, 0))
+	var defaultX = -600
+	for i in range(len(ans)):
+		spawn(Vector2(defaultX, -300), i)
+		defaultX += space
+
+func spawn(pos, name):
 	var p = partsScene.instantiate()
 	add_child(p)
 	p.global_position = pos
-	
+	p.name = str(name)
 	p.dropped.connect(_partDropped)
 	
 func _partDropped(partNode):
 	print(partNode.name)
-	if partNode.global_position.distance_to(ans) < 10:
-		partNode.global_position = ans
+	if partNode.global_position.distance_to(ans[int(partNode.name)]) < 10:
+		partNode.global_position = ans[int(partNode.name)]
 		print("正解")
+		# 正解したらドラッグ不可に
+		partNode.set_process_input(false)
+	else:
+		partNode.global_position = partNode.defaultIdx
