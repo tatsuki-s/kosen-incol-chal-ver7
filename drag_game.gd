bcbcbf04006e9bfc9d22ec7d16aad86d9634b40f
tatsuki-s <zekoushn@gmail.com>
2026-02-22 12:00:57 +0900

    ちゃんとドラッグを実装

--- drag_game.gd
+++ drag_game.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var parts = $Parts
 @onready var posi = $position
-
+@onready var area2D = $Parts/Area2D
 
 var dragIdx = [0, 0]
 var isDraging = false
@@ -12,10 +12,21 @@ var area = Rect2(64, 64, 1152, 576)
 
 func _ready() -> void:
 	var screen = get_parent().screen
+	area2D.input_event.connect(_onArea2dInput)
 	print(parts.position)
 
+func _onArea2dInput(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton or event is InputEventScreenTouch:
+		if event.pressed:
+			isDraging = true
+	
+
 func _input(event):
-	if event is InputEventScreenDrag:
+	if isDraging and ( event is InputEventMouseButton or event is InputEventScreenTouch):
+		if not event.pressed:
+			isDraging = false
+	
+	if isDraging and event is InputEventScreenDrag:
 		var localPos = get_viewport().get_mouse_position()
 		var posX = snapped(localPos.x, dragSpace)
 		var posY = snapped(localPos.y, dragSpace)
